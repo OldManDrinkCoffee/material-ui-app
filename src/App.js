e73807aab050323be2d72b218201203a13bd96bb
Danh <nguyenhoangcongdanh2075@gmail.com>
2024-06-06 16:29:02 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import PrimarySearchAppBar from "./components/AppBar";
+import { Box, Container, Grid, createTheme } from "@mui/material";
+import jobs from "./jobs.json";
+import JobCard from "./components/JobCard";
+import Pagination from "@mui/material/Pagination";
+import { ThemeProvider } from "@emotion/react";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#b53f51",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Box style={{ background: "black" }}>
+        <PrimarySearchAppBar />
+        <Container style={{ background: "black" }} maxWidth="lg">
+          <Grid
+            style={{ marginTop: "1rem", display: "flex" }}
+            container
+            spacing={2}
+          >
+            {jobs?.map((job) => (
+              <Grid key={job.id} item xs={12} md={4}>
+                <JobCard job={job} />
+              </Grid>
+            ))}
+          </Grid>
+          <Grid
+            style={{
+              marginTop: "1rem",
+              display: "flex",
+              justifyContent: "center",
+            }}
+            item
+            xs={12}
+            md={12}
+          >
+            <Pagination
+              style={{
+                color: "black",
+                width: "100%",
+                justifyContent: "center",
+                display: "flex",
+              }}
+              count={10}
+              color="secondary"
+            />
+          </Grid>
+        </Container>
+      </Box>
+    </ThemeProvider>
   );
 }
 
